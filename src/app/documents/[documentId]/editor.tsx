03f1a6c255d9d32b4a4ed4f61d58191af74ce781
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-29 23:54:36 +0530

    feat(editor): integrate TipTap editor with rich text extensions and state store

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { TaskItem, TaskList } from "@tiptap/extension-list";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { TableKit } from "@tiptap/extension-table";
+// import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+import { useEditorStore } from "@/store/use-editor-store";
+import { FontFamily, TextStyle, Color } from "@tiptap/extension-text-style";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
+import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
+import { Ruler } from "./ruler";
+
+export const Editor = () => {
+  const { setEditor } = useEditorStore();
+
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right:56px",
+        class:
+          "tiptap focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      },
+    },
+    extensions: [
+      StarterKit.configure({
+        heading: {
+          levels: [1, 2, 3, 4, 5],
+        },
+      }),
+      TableKit.configure({
+        table: { resizable: true },
+      }),
+      TaskList,
+      FontSizeExtension,
+      TextStyle,
+      FontFamily,
+      LineHeightExtension.configure({
+        types: ["heading", "paragraph"],
+        defaultLineHeight: "normal",
+      }),
+      // Image,
+      Color,
+      Highlight.configure({
+        multicolor: true,
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
+      ImageResize.configure({
+        allowBase64: true,
+      }),
+      TaskItem.configure({
+        nested: true,
+      }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    immediatelyRender: false,
+  });
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
