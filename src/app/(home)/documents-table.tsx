81942c61c1be37ea564915c5e78b0e382e0f767a
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-30 23:13:43 +0530

    feat(documents): render paginated documents table with empty state and load-more support

---
+++ src/app/(home)/documents-table.tsx
@@ -0,0 +1,78 @@
+import { PaginationStatus } from "convex/browser";
+import { Doc } from "../../../convex/_generated/dataModel";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { LoaderIcon } from "lucide-react";
+import DocumentRow from "./document-row";
+import { Button } from "@/components/ui/button";
+
+interface DocumentsTableProps {
+  documents: Doc<"documents">[] | undefined;
+  loadMore: (numItems: number) => void;
+  status: PaginationStatus;
+}
+
+export const DocumentsTable = ({
+  documents,
+  loadMore,
+  status,
+}: DocumentsTableProps) => {
+  return (
+    <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5">
+      {documents === undefined ? (
+        <div className="flex justify-center items-center h-24">
+          <LoaderIcon className="animate-spin text-muted-foreground size-5" />
+          {/* <FullscreenLoader label="Loading Documents....." /> */}
+        </div>
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>&nbsp;</TableHead>
+              <TableHead className="hidden md:table-cell">Shared</TableHead>
+              <TableHead className="hidden md:table-cell">Created at</TableHead>
+            </TableRow>
+          </TableHeader>
+          {documents.length === 0 ? (
+            <TableBody>
+              <TableRow className="hover:bg-transparent">
+                <TableCell
+                  colSpan={4}
+                  className="h-24 text-center text-muted-foreground"
+                >
+                  No documents found
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          ) : (
+            <TableBody>
+              {documents.map((document) => (
+                <DocumentRow key={document._id} document={document} />
+              ))}
+            </TableBody>
+          )}
+        </Table>
+      )}
+
+      <div className="flex items-center justify-center">
+        <Button
+          variant={`ghost`}
+          size={`sm`}
+          onClick={() => loadMore(5)}
+          disabled={status !== "CanLoadMore"}
+        >
+          {status === "CanLoadMore" ? "Load More" : "End of results"}
+        </Button>
+      </div>
+    </div>
+  );
+};
