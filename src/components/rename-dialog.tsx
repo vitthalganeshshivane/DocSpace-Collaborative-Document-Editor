b5b81ea428149bb746fb9fd856ca4c6fab9b1e24
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-30 23:06:40 +0530

    feat(documents): add rename dialog with optimistic update and toast feedback

---
+++ src/components/rename-dialog.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Id } from "../../convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { toast } from "sonner";
+
+interface RenameDialogProps {
+  documentId: Id<"documents">;
+  children: React.ReactNode;
+  initialTitle: string;
+}
+
+export const RenameDialog = ({
+  documentId,
+  initialTitle,
+  children,
+}: RenameDialogProps) => {
+  const update = useMutation(api.documents.updateById);
+
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [title, setTitle] = useState(initialTitle);
+  const [open, setOpen] = useState(false);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsUpdating(true);
+
+    update({ id: documentId, title: title.trim() || "Untitled" })
+      .then(() => setOpen(false))
+      .catch(() => toast.error("something went wrong"))
+      .then(() => toast.success("Document Updated"))
+      .finally(() => {
+        setIsUpdating(false);
+      });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <form onSubmit={onSubmit}>
+          <DialogHeader>
+            <DialogTitle>Rename document</DialogTitle>
+            <DialogDescription>
+              Enter a new name for this document
+            </DialogDescription>
+          </DialogHeader>
+          <div className="my-4">
+            <Input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Document name"
+              onClick={(e) => e.stopPropagation()}
+            />
+          </div>
+          <DialogFooter>
+            <Button
+              type="button"
+              variant={`ghost`}
+              disabled={isUpdating}
+              onClick={(e) => {
+                e.stopPropagation();
+                setOpen(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              disabled={isUpdating}
+              onClick={(e) => e.stopPropagation()}
+            >
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
