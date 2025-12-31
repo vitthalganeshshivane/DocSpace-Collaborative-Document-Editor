6ced5627c7c4b667e72d1a103f16f9624c9515d8
vitthal4545 <vitthalgs1434@gmail.com>
2026-01-01 02:09:30 +0530

    refactor(document-page): simplify document preload flow and remove redundant check

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -27,10 +27,6 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
     { token }
   );
 
-  if (!preloadedDocument) {
-    throw new Error("Document not found");
-  }
-
   return <Document preloadedDocument={preloadedDocument} />;
 };
 
