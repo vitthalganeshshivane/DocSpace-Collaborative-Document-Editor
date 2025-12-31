115534aa8f5a3cc91e07de6118216b62ccff3fec
vitthal4545 <vitthalgs1434@gmail.com>
2026-01-01 02:05:17 +0530

    feat(document): add live title editing with debounced autosave and sync status

---
+++ src/app/documents/[documentId]/document-input.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { BsCloudCheck } from "react-icons/bs";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useDebounce } from "@/hooks/use-debounce";
+import { toast } from "sonner";
+import { useStatus } from "@liveblocks/react";
+import { LoaderIcon } from "lucide-react";
+
+interface DocumentInputProps {
+  title: string;
+  id: Id<"documents">;
+}
+
+export const DocumentInput = ({ title, id }: DocumentInputProps) => {
+  const status = useStatus();
+
+  const [value, setValue] = useState(title);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const mutate = useMutation(api.documents.updateById);
+
+  const debouncedUpdate = useDebounce((newValue: string) => {
+    if (newValue === title) return;
+
+    setIsPending(true);
+    mutate({ id, title: newValue })
+      .then(() => toast.success("Document Updated"))
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    debouncedUpdate(newValue);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsPending(true);
+    mutate({ id, title: value })
+      .then(() => {
+        toast.success("Document Updated");
+        setIsEditing(false);
+      })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
+  };
+
+  const showLoader =
+    isPending || status === "connecting" || status === "reconnecting";
+  const showError = status == "disconnected";
+
+  return (
+    <div className="flex items-center gap-2">
+      {isEditing ? (
+        <form onSubmit={handleSubmit} className="relative w-fit max-w-[50ch]">
+          <span className="invisible whitespace-pre px-1.5 text-lg">
+            {value || ""}
+          </span>
+          <input
+            ref={inputRef}
+            value={value}
+            onBlur={() => setIsEditing(false)}
+            onChange={onChange}
+            className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"
+          />
+        </form>
+      ) : (
+        <span
+          onClick={() => {
+            setIsEditing(true);
+            setTimeout(() => {
+              inputRef.current?.focus();
+            }, 0);
+          }}
+          className="text-lg px-1.5 cursor-pointer truncate"
+        >
+          {title}
+        </span>
+      )}
+
+      {showError && <BsCloudCheck className="size-4" />}
+      {!showError && !showLoader && <BsCloudCheck />}
+      {showLoader && (
+        <LoaderIcon className="size-4 animate-spin text-muted-foreground" />
+      )}
+    </div>
+  );
+};
