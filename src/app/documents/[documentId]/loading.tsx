85dddd46bf32b844706895e7411bbe29ec2358a8
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-31 23:54:16 +0530

    feat(loading): add fullscreen document loading state

---
+++ src/app/documents/[documentId]/loading.tsx
@@ -0,0 +1,7 @@
+import { FullscreenLoader } from "@/components/fullscreen-loader";
+
+const LoadingPage = () => {
+  return <FullscreenLoader label="Document Loading" />;
+};
+
+export default LoadingPage;
