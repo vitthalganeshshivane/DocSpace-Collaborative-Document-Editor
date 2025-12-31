8fcfd42677dac309761bfa6021e019b3f21fc471
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-31 23:56:26 +0530

    feat(document): preload document data with Convex auth

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,37 @@
+import { preloadQuery } from "convex/nextjs";
+import { auth } from "@clerk/nextjs/server";
+
+import { Document } from "./document";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { api } from "../../../../convex/_generated/api";
+
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: Id<"documents"> }>;
+}
+
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+
+  const { getToken } = await auth();
+  const token = (await getToken({ template: "convex" })) ?? undefined;
+
+  if (!token) {
+    throw new Error("Unathorized");
+  }
+
+  const preloadedDocument = await preloadQuery(
+    api.documents.getById,
+    {
+      id: documentId,
+    },
+    { token }
+  );
+
+  if (!preloadedDocument) {
+    throw new Error("Document not found");
+  }
+
+  return <Document preloadedDocument={preloadedDocument} />;
+};
+
+export default DocumentIdPage;
